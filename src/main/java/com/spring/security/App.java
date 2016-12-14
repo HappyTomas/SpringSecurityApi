ec308e3a1fd3fdfa38bb6a81f28a80cdcbe21b1b
奈斯曼 <81303012@qq.com>
2016-12-14 10:02:37 +0800

    修改端口号为8081

--- src/main/java/com/spring/security/App.java
+++ src/main/java/com/spring/security/App.java
@@ -2,11 +2,29 @@ package com.spring.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class,args);
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+
+    /**
+     * 配置信息
+     * @param container
+     */
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8081);
     }
 }
